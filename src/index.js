df44bb89baf25d9bcc2d329e24397a5092d767ce
Yash Sharma <yash9457sharma@gmail.com>
2026-06-21 02:03:15 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// requie("dotenv").config({path: "./env"});
+
+import dotenv from "dotenv";
+
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({path: "./env"});
+connectDB();
+
+
+
+
+
+
+
+/*
+
+import express from "express";
+const app = express();
+
+
+;(async ()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error" , (error)=>{
+        console.log("ERROR:" , error);
+        throw error 
+       })
+
+       app.listen(process.env.PORT , ()=>{
+        console.log(`app is running on port ${process.env.PORT}`);
+       })
+       
+    }
+    catch(err){
+        console.error("ERROR: " , err)
+        throw err;
+    }
+}) ()
+
+*/
+
+
+
+
