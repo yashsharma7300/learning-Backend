0e0468975166be6acff5557e3c844ca6e961951a
Yash Sharma <yash9457sharma@gmail.com>
2026-06-21 03:37:39 +0530

    utilis code making

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-// requie("dotenv").config({path: "./env"});
+// require("dotenv").config({path: "./env"});
 
 import dotenv from "dotenv";
-
 
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants.js";
 import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({ path: "./env" });
 
 
-dotenv.config({path: "./env"});
-connectDB();
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.on("error" , (error)=>{
+        console.log("error :" , error);
+        throw error ;
+    })
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(`server is running at port : ${proceess.env.PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.log("mongodb connection failed!! ", error);
+  });
 
 /*
 
@@ -43,7 +50,3 @@ const app = express();
 }) ()
 
 */
-
-
-
-
