1a54474a628a8cfabd677b525ace4c361915839f
Yash Sharma <yash9457sharma@gmail.com>
2026-06-22 03:09:31 +0530

    router changees

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,12 +2,14 @@ import express from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import upload from "../utils/multer.js";
 
-const router  = express.Router();
+const router = express.Router();
 
-router.route('/register').post(registerUser);
-
-
-
-
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 export default router;
